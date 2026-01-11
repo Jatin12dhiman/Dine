98f416d856e5b6b3b1e12f6e6829b0c968badf51
Jatin Dhiman <dhimanjatin003@gmail.com>
2026-01-11 17:17:10 +0530

    Image scroll

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,6 +1,6 @@
 import { BlurView } from 'expo-blur';
 import { useRouter } from "expo-router";
-import { ActivityIndicator, FlatList, Image, ImageBackground, Platform, ScrollView, Text, View } from 'react-native';
+import { ActivityIndicator, FlatList, Image, ImageBackground, Platform, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import logo from "../../assets/images/dinetimelogo.png";
 import banner from "../../assets/images/homeBanner.png";
@@ -9,9 +9,43 @@ import { restaurants } from "../../store/restaurants";
 const home = () => {
   const router = useRouter();
 
+
+  const renderItem = ({ item }) => (
+  <TouchableOpacity
+    className="bg-[#5f5f5f] w-72 rounded-lg p-4 mx-2 shadow-md overflow-hidden"
+  >
+    <Image
+      source={{ uri: item.image }}
+      resizeMode="cover"
+      className="w-full h-28 rounded-lg mb-2"
+    />
+
+    <Text
+      className="text-white text-lg font-bold mb-1"
+      numberOfLines={1}
+    >
+      {item.name}
+    </Text>
+
+    <Text
+      className="text-white text-sm mb-1"
+      numberOfLines={2}
+    >
+      {item.address}
+    </Text>
+
+    <Text
+      className="text-white text-sm"
+      numberOfLines={2}
+    >
+      Opening: {item.opening} – Closing: {item.closing}
+    </Text>
+  </TouchableOpacity>
+);
+
   return (
     <SafeAreaView style={{ backgroundColor: "#2b2b2b" }}>
-      <View className="flex items-center mt-1">
+      <View className="flex items-center ">
         <View className="bg-[#5f5f5f] w-11/12 rounded-lg shadow-lg justify-between items-center flex flex-row p-2">
           <View className="flex flex-row">
             <Text className={`text-base h-10 pt-[${Platform.OS === "ios" ? 8 : 6.5}] align-middle text-white `}>{" "}
@@ -20,10 +54,10 @@ const home = () => {
           </View>
         </View>
       </View>
-      <ScrollView>
+      <ScrollView stickyHeaderIndices={[0]}>
         <ImageBackground
           resizeMode="cover"
-          className="my-4 w-full h-52 items-center justify-center"
+          className="my-4 w-full bg-[#2b2b2b] h-52 items-center justify-center mb-4"
           source={banner}
         >
           <BlurView tint="dark"
@@ -35,12 +69,21 @@ const home = () => {
             </Text>
           </BlurView>
         </ImageBackground>
+        {
+          restaurants.length > 0 ? (
+            <FlatList
+              data={restaurants}
+              renderItem={renderItem}
+              horizontal
+              contentContainerStyle={{ padding: 16 }} showHorizontalScrollIndicator={false} scrollEnabled={true}
+            />) : (
+            <ActivityIndicator animating color={"#fb9b33"} />
+          )
+        }
+        
+
       </ScrollView>
 
-      {
-        restaurants.length > 0 ?
-          <FlatList /> : <ActivityIndicator animating color={"#fb9b33"} />
-      }
 
     </SafeAreaView>
   )
