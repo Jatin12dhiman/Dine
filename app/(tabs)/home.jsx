a3720311042d54be1ce3df3f6b9a3e944d503bfc
Jatin Dhiman <dhimanjatin003@gmail.com>
2026-01-11 14:40:06 +0530

    st2

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,10 +1,14 @@
-import { Image, Text, View, Platform, ScrollView } from 'react-native';
+import { BlurView } from 'expo-blur';
+import { useRouter } from "expo-router";
+import { ActivityIndicator, FlatList, Image, ImageBackground, Platform, ScrollView, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import logo from "../../assets/images/dinetimelogo.png";
-import { ImageBackground } from 'react-native';
-import { BlurView } from 'expo-blur';
+import banner from "../../assets/images/homeBanner.png";
+import { restaurants } from "../../store/restaurants";
 
 const home = () => {
+  const router = useRouter();
+
   return (
     <SafeAreaView style={{ backgroundColor: "#2b2b2b" }}>
       <View className="flex items-center mt-1">
@@ -13,7 +17,6 @@ const home = () => {
             <Text className={`text-base h-10 pt-[${Platform.OS === "ios" ? 8 : 6.5}] align-middle text-white `}>{" "}
               Welcome to {""}</Text>
             <Image resizeMode="cover" className={"w-20 h-11"} source={logo} />
-
           </View>
         </View>
       </View>
@@ -21,16 +24,24 @@ const home = () => {
         <ImageBackground
           resizeMode="cover"
           className="my-4 w-full h-52 items-center justify-center"
-          source
-          >
+          source={banner}
+        >
           <BlurView tint="dark"
-            intensity={100}>
-            <Text className="text-center text-3xl font-bold text-white" >
+            intensity={Platform.OS === "android" ? 100 : 50}
+            className="w-full p-4 shadow-lg"
+          >
+            <Text className="text-center text-3xl font-bold text-white">
               Dine with your loved ones
             </Text>
           </BlurView>
         </ImageBackground>
       </ScrollView>
+
+      {
+        restaurants.length > 0 ?
+          <FlatList /> : <ActivityIndicator animating color={"#fb9b33"} />
+      }
+
     </SafeAreaView>
   )
 }
