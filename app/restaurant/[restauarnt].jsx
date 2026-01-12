bfa4e602f868f64a3fa8a153f1f6aa0a816df530
Jatin Dhiman <dhimanjatin003@gmail.com>
2026-01-12 18:47:02 +0530

    carousel

--- app/restaurant/[restauarnt].jsx
+++ app/restaurant/[restauarnt].jsx
@@ -1,9 +1,44 @@
 import { useLocalSearchParams } from 'expo-router';
 import { Platform, ScrollView, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useState } from 'react';
+import { where } from 'firebase/firestore';
 
 export default function Restaurant() {
     const { restauarnt } = useLocalSearchParams();
+
+    const[restaurantData , setRestaurantData] = useState({})
+    const[carouselData , setCarouselData] = useState({})
+    const[slotsData , setSlotsData] = useState({})
+
+    const getRestaurantData = async () => {
+        try{
+            const restaurantQuery = query(collection(db , "restaurants"),where("name","==",restauarnt))
+            const restaurantSnapshot = await getDocs(restaurantQuery)
+            
+            if(restaurantSnapshot.empty){
+                console.log("No matching restaurant found")
+                return ;
+            }
+            for(const doc of restaurantSnapshot.docs){
+                const restaurantData = doc.data();
+                setRestaurantData(restaurantData)
+
+                const carouselQuery = query(collection(db , "carousel"),where("res_id","==", doc.ref)
+             );
+             const carouselSnapshot = await getDocs(carouselQuery);
+             const carouselImages = [];
+             carouselSnapshot.forEach((carouselDoc)=>{
+                carouselImages.push(carouselDoc.data())
+             })
+             setCarouselData(carouselImages)
+
+            }
+        }catch(error){
+            console.log(error)
+        }
+    }
+     
 
     return (
         <SafeAreaView style={[{ backgroundColor: "#2b2b2b" },
@@ -12,7 +47,7 @@ export default function Restaurant() {
         ]}>
             <ScrollView className="h-full">
                 <View className="flex">
-                    <Text className="text-xl text-[#f49b33] mr-2 font-semibold">{restauarnt}</Text>
+                    <Text className="text-xl text-[#f49b33] mr-2 mt-[12px] font-semibold">{restauarnt}</Text>
                      
                     <View className="border-b border-[#f49b33] "></View>
                 </View>
