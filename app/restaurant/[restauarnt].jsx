087c0824d449cdbb606154275c5f96a44d27fe36
Jatin Dhiman <dhimanjatin003@gmail.com>
2026-01-12 17:35:30 +0530

    res2

--- app/restaurant/[restauarnt].jsx
+++ app/restaurant/[restauarnt].jsx
@@ -1,13 +1,22 @@
 import { useLocalSearchParams } from 'expo-router';
-import { Text } from 'react-native';
+import { Platform, ScrollView, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Restaurant() {
     const { restauarnt } = useLocalSearchParams();
 
     return (
-        <SafeAreaView>
-            <Text>{restauarnt}</Text>
+        <SafeAreaView style={[{ backgroundColor: "#2b2b2b" },
+        Platform.OS == "android" && { paddingBottom: 10 },
+        Platform.OS == "ios" && { paddingBottom: 20 },
+        ]}>
+            <ScrollView className="h-full">
+                <View className="flex">
+                    <Text className="text-xl text-[#f49b33] mr-2 font-semibold">{restauarnt}</Text>
+                     
+                    <View className="border-b border-[#f49b33] "></View>
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
