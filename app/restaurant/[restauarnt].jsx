8e25f360730658e35af01a703fc5e2f0040cc1f0
Jatin Dhiman <dhimanjatin003@gmail.com>
2026-01-13 16:43:56 +0530

    arrow

--- app/restaurant/[restauarnt].jsx
+++ app/restaurant/[restauarnt].jsx
@@ -14,25 +14,67 @@ export default function Restaurant() {
     const [restaurantData, setRestaurantData] = useState({})
     const [carouselData, setCarouselData] = useState({})
     const [slotsData, setSlotsData] = useState({})
+    const [currentIndex, setCurrentIndex] = useState(0)
 
-    const carouselItem = ({item}) => {
-        return(
-            <View style={{width:windowWidth-2}} className="h-64 relative rounded-[25px]">
-                <View style={{position:"absolute", top:"50%",
-                     backgroundColor:"rgba(0,0,0,0.6)", borderRadius:50,
-                     padding:5,
-                     zIndex:10,
-                     right:"6%"}}>
-                    <Ionicons name="arrow-forward" size={24} color="white" />
+    const handleNextImage = () => {
+        const carouselLength = carouselData[0]?.images.length;
+        if (currentIndex < carouselLength - 1) {
+            const nextIndex = currentIndex + 1;
+            setCurrentIndex(nextIndex);
+            flatListRef.current.scrollToIndex({ index: nextIndex, animated: true })
+        }
+        if (currentIndex == carouselLength - 1) {
+            const nextIndex = 0;
+            setCurrentIndex(nextIndex)
+            flatListRef.current.scrollToIndex({ index: nextIndex, animated: true })
+        }
+    }
+
+    const handlePrevImage = () => {
+        const carouselLength = carouselData[0]?.images.length;
+        if (currentIndex > 0) {
+            const prevIndex = currentIndex - 1;
+            setCurrentIndex(prevIndex);
+            flatListRef.current.scrollToIndex({ index: prevIndex, animated: true })
+        }
+        if (currentIndex == 0) {
+            const prevIndex = carouselLength - 1;
+            setCurrentIndex(prevIndex)
+            flatListRef.current.scrollToIndex({ index: prevIndex, animated: true })
+        }
+    }
+
+    const carouselItem = ({ item }) => {
+        return (
+            <View style={{ width: windowWidth - 2 }} className="h-64 relative ">
+                <View
+                    style={{
+                        position: "absolute", top: "50%",
+                        backgroundColor: "rgba(0,0,0,0.6)", borderRadius: 50,
+                        padding: 5,
+                        zIndex: 10,
+                        right: "6%"
+                    }}>
+                    <Ionicons onPress={handleNextImage} name="arrow-forward" size={24} color="white" />
+                </View>
+                <View
+                    style={{
+                        position: "absolute", top: "50%",
+                        backgroundColor: "rgba(0,0,0,0.6)", borderRadius: 50,
+                        padding: 5,
+                        zIndex: 10,
+                        left: "6%"
+                    }}>
+                    <Ionicons onPress={handlePrevImage} name="arrow-back" size={24} color="white" />
                 </View>
                 <View>
-                        <Image source={{uri:item}} 
+                    <Image source={{ uri: item }}
                         style={{
-                            top:"5%",
-                            opacity:0.5, backgroundColor:"black", 
-                            marginRight:15, 
-                            marginLeft:15,
-                            borderRadius:25,
+                            top: "5%",
+                            opacity: 0.5, backgroundColor: "black",
+                            marginRight: 15,
+                            marginLeft: 15,
+                            borderRadius: 25,
                         }}
                         className="h-64" />
                 </View>
@@ -41,34 +83,34 @@ export default function Restaurant() {
     }
 
     const getRestaurantData = async () => {
-        try{
-            const restaurantQuery = query(collection(db , "restaurants"),where("name","==",restauarnt))
+        try {
+            const restaurantQuery = query(collection(db, "restaurants"), where("name", "==", restauarnt))
             const restaurantSnapshot = await getDocs(restaurantQuery)
 
-            if(restaurantSnapshot.empty){
+            if (restaurantSnapshot.empty) {
                 console.log("No matching restaurant found")
-                return ;
+                return;
             }
-            for(const doc of restaurantSnapshot.docs){
+            for (const doc of restaurantSnapshot.docs) {
                 const restaurantData = doc.data();
                 setRestaurantData(restaurantData)
 
-                const carouselQuery = query(collection(db , "carousel"),where("res_id","==", doc.ref)
+                const carouselQuery = query(collection(db, "carousel"), where("res_id", "==", doc.ref)
                 );
                 const carouselSnapshot = await getDocs(carouselQuery);
                 const carouselImages = [];
 
-                if(carouselSnapshot.empty){
+                if (carouselSnapshot.empty) {
                     console.log("No matching carousel found")
-                    return ;
+                    return;
                 }
 
-                carouselSnapshot.forEach((carouselDoc)=>{
+                carouselSnapshot.forEach((carouselDoc) => {
                     carouselImages.push(carouselDoc.data())
                 })
                 setCarouselData(carouselImages)
 
-                const slotsQuery = query(collection(db , "slots"),where("ref_id","==", doc.ref)
+                const slotsQuery = query(collection(db, "slots"), where("ref_id", "==", doc.ref)
                 );
                 const slotsSnapshot = await getDocs(slotsQuery);
                 const slots = [];
@@ -108,7 +150,10 @@ export default function Restaurant() {
                         data={carouselData[0]?.images}
                         renderItem={carouselItem}
                         horizontal
+                        scrollEnabled={false}
+
                         showsHorizontalScrollIndicator={false}
+                        style={{ borderRadius: 25 }}
                     />
                 </View>
             </ScrollView>
